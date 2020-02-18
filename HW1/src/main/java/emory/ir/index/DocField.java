63cac427adb13c3b1d6f587eeb4717cb79c5de3d
lxucs <leonxu624@gmail.com>
2020-02-17 22:53:36 -0500

    Implemented index baseline

---
+++ HW1/src/main/java/emory/ir/index/DocField.java
@@ -0,0 +1,8 @@
+package emory.ir.index;
+
+public class DocField {
+
+    public static String DOC_NO = "docno";
+    public static String TEXT = "text";
+
+}
--- HW1/src/main/java/emory/ir/index/IndexFiles.java
+++ HW1/src/main/java/emory/ir/index/IndexFiles.java
@@ -1,26 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package emory.ir.index;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -32,7 +15,6 @@ import java.util.Date;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -43,6 +25,8 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Index all text files under a directory.
@@ -114,7 +98,7 @@ public class IndexFiles {
             // iwc.setRAMBufferSizeMB(256.0);
 
             IndexWriter writer = new IndexWriter(dir, iwc);
-            indexDocs(writer, docDir);
+            indexFiles(writer, docDir);
 
             // NOTE: if you want to maximize search performance,
             // you can optionally call forceMerge here.  This can be
@@ -129,86 +113,105 @@ public class IndexFiles {
             Date end = new Date();
             System.out.println(end.getTime() - start.getTime() + " total milliseconds");
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(" caught a " + e.getClass() +
                     "\n with message: " + e.getMessage());
         }
     }
 
-    /**
-     * Indexes the given file using the given writer, or if a directory is given,
-     * recurses over files and directories found under the given directory.
-     * <p>
-     * NOTE: This method indexes one document per input file.  This is slow.  For good
-     * throughput, put multiple documents into your input file(s).  An example of this is
-     * in the benchmark module, which can create "line doc" files, one document per line,
-     * using the
-     * <a href="../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
-     * >WriteLineDocTask</a>.
-     *
-     * @param writer Writer to the index where the given file/dir info will be stored
-     * @param path   The file to index, or the directory to recurse into to find files to index
-     * @throws IOException If there is a low-level I/O error
-     */
-    static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+    public static void indexFiles(final IndexWriter writer, Path path) throws Exception {
         if (Files.isDirectory(path)) {
             Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     try {
-                        indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
-                    } catch (IOException ignore) {
-                        // don't index files that can't be read.
+                        indexDocs(writer, file);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                     return FileVisitResult.CONTINUE;
                 }
             });
-        } else {
-            indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+        } else
+            indexDocs(writer, path);
+    }
+
+    public static void indexDocs(IndexWriter writer, Path file) throws Exception {
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        // Ignore non-document file
+        String filename = file.getFileName().toString();
+        if(filename.endsWith(".z") || filename.contains("read") || (!filename.contains("fb") && !filename.contains("ft") && !filename.contains("la"))) {
+            System.out.println("Ignore file: " + file);
+            return;
+        }
+
+        try(InputStream stream = Files.newInputStream(file)) {
+            // Read each line to identify each document
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                if(line.trim().isEmpty())
+                    continue;
+
+                if(line.trim().equals("<DOC>")) {
+                    // New document
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(line);
+                    while(true) {
+                        line = reader.readLine();
+                        sb.append(line);
+                        if(line.trim().equals("</DOC>")) {
+                            // Parse the document
+                            String docStr = sb.toString();
+                            InputStream docStream = new ByteArrayInputStream(docStr.getBytes(StandardCharsets.UTF_8));
+                            org.w3c.dom.Document docDom = db.parse(docStream);
+
+                            // Index document
+                            Document doc = indexDoc(docDom);
+                            if(doc == null) {
+                                System.out.println("Skip: no DOCNO or TEXT in document");
+                                break;
+                            }
+
+                            // Write document
+                            if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+                                System.out.println("adding " + doc.get(DocField.DOC_NO));
+                                writer.addDocument(doc);
+                            } else {
+                                System.out.println("updating " + doc.get(DocField.DOC_NO));
+                                writer.updateDocument(new Term("path", file.toString()), doc);
+                            }
+                            break;
+                        }
+                    }
+                } else
+                    throw new RuntimeException("Invalid doc start tag: " + file);
+            }
         }
     }
 
-    /**
-     * Indexes a single document
-     */
-    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
-        try (InputStream stream = Files.newInputStream(file)) {
-            // make a new, empty document
-            Document doc = new Document();
+    private static Document indexDoc(org.w3c.dom.Document docDom) {
+        Document doc = new Document();
 
-            // Add the path of the file as a field named "path".  Use a
-            // field that is indexed (i.e. searchable), but don't tokenize
-            // the field into separate words and don't index term frequency
-            // or positional information:
-            Field pathField = new StringField("path", file.toString(), Field.Store.YES);
-            doc.add(pathField);
+        String docNo = null, text = null;
 
-            // Add the last modified date of the file a field named "modified".
-            // Use a LongPoint that is indexed (i.e. efficiently filterable with
-            // PointRangeQuery).  This indexes to milli-second resolution, which
-            // is often too fine.  You could instead create a number based on
-            // year/month/day/hour/minutes/seconds, down the resolution you require.
-            // For example the long value 2011021714 would mean
-            // February 17, 2011, 2-3 PM.
-            doc.add(new LongPoint("modified", lastModified));
+        Node node = docDom.getElementsByTagName("DOCNO").item(0);
+        if(node != null)
+            docNo = node.getTextContent().trim();
 
-            // Add the contents of the file to a field named "contents".  Specify a Reader,
-            // so that the text of the file is tokenized and indexed, but not stored.
-            // Note that FileReader expects the file to be in UTF-8 encoding.
-            // If that's not the case searching for special characters will fail.
-            doc.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+        node = docDom.getElementsByTagName("TEXT").item(0);
+        if(node != null)
+            text = node.getTextContent().trim();
 
-            if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-                // New index, so we just add the document (no old document can be there):
-                System.out.println("adding " + file);
-                writer.addDocument(doc);
-            } else {
-                // Existing index (an old copy of this document may have been indexed) so
-                // we use updateDocument instead to replace the old one matching the exact
-                // path, if present:
-                System.out.println("updating " + file);
-                writer.updateDocument(new Term("path", file.toString()), doc);
-            }
-        }
+        if(docNo == null || text == null)
+            return null;
+
+        Field docIdField = new StringField(DocField.DOC_NO, docNo, Field.Store.YES);
+        Field textField = new TextField(DocField.TEXT, text, Field.Store.NO);
+
+        doc.add(docIdField);
+        doc.add(textField);
+
+        return doc;
     }
 }
