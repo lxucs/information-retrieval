2d397f08cca75d17815571e5c3e436c1fd33cfc7
Thiago Santos <thiagogyn1@hotmail.com>
2020-02-18 21:01:16 -0500

    Score Laplace

--- HW1/src/main/java/emory/ir/search/LMLaplace.java
+++ HW1/src/main/java/emory/ir/search/LMLaplace.java
@@ -3,7 +3,7 @@ package emory.ir.search;
 
 import org.apache.lucene.search.similarities.BasicStats;
 import org.apache.lucene.search.similarities.LMSimilarity;
-
+import org.apache.lucene.search.CollectionStatistics;
 
 
 public  class LMLaplace extends LMSimilarity {
@@ -11,14 +11,14 @@ public  class LMLaplace extends LMSimilarity {
 
     @Override
     public String getName() {
-        return null;
+        return this.getClass().getSimpleName();
     }
 
     @Override
     protected double score(BasicStats stats, double freq, double docLen) {
 
         // add 1 to every count and then normalize
-
-        return 1;
+        ((LMStats)stats).getCollectionProbability();
+        return (double)(1 + freq) / (doclen + stats.getNumberOfFieldTokens());
     }
 }
